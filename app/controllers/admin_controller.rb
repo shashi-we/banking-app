80276364534203a1d1f7e88ea821197ee8f69bcf
Shashi Singh <shashi.we@gmail.com>
2019-06-07 19:21:04 +0530

    seed some records and have transactions grouped by account number

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -15,7 +15,9 @@ class AdminController < ApplicationController
 
   def transaction_report
     account_ids = params[:account_ids].map(&:to_i) unless params[:account_ids].nil?
-    @transactions = BankTransaction.where("date(transaction_time) >= ? AND date(transaction_time) <= ?", params[:start_date], params[:end_date]).where(bank_account_id: account_ids).includes(:bank_account).order(:bank_account_id)
+    @transactions = BankTransaction.where("date(transaction_time) >= ? AND date(transaction_time) <= ?", params[:start_date], params[:end_date]).where(bank_account_id: account_ids).includes(:bank_account)
+
+    @account_transactions = @transactions.group_by { |t| t.bank_account.account_number }
     
     if @transactions.empty?
       redirect_to transaction_histories_path, notice: 'No transactions found.'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,58 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+  name: "Admin",
+  email: "admin@test.com", 
+  password: "1234", 
+  password_confirmation: "1234",
+  roles: "admin"
+)
+puts '1 admin is created'
+
+5.times do |i|
+  User.create!(
+    name: "User #{i+1}",
+    email: "user#{i+1}@test.com", 
+    password: "password", 
+    password_confirmation: "password",
+    roles: "customer", 
+    address: "User #{i+1} address"
+  )
+end
+puts '5 customers are created'
+
+2.times do |i|
+  BankAccount.first.bank_transactions.create!(
+    transaction_type: "credit",
+    description: "adding money",
+    transaction_amount: 100,
+    transaction_time: Time.now
+  )
+  BankAccount.find(i+3).bank_transactions.create!(
+    transaction_type: "credit",
+    description: "first deposit",
+    transaction_amount: 90,
+    transaction_time: Time.now
+  )
+  BankAccount.last.bank_transactions.create!(
+    transaction_type: "credit",
+    description: "for saving",
+    transaction_amount: 120,
+    transaction_time: Time.now
+  )
+end
+puts '6 credit transactions added'
+
+3.times do |i|
+  BankAccount.first.bank_transactions.create!(
+    transaction_type: "debit",
+    description: "emergency withdrawl",
+    transaction_amount: 35,
+    transaction_time: Time.now
+  )
+  BankAccount.last.bank_transactions.create!(
+    transaction_type: "debit",
+    description: "withdrawing for shopping",
+    transaction_amount: 40,
+    transaction_time: Time.now
+  )
+end
+puts '6 debit transactions added'
